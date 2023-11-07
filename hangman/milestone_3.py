498fdfa15071c6c0b1742aa1ac67525d551c955d
Emma Luk <emma_luk1@hotmail.com>
2023-11-07 16:03:09 +0000

    add two functions, check_guess and ask_for_input

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,19 +1,26 @@
-# Previous code for input validation
-while True:
-    guess = input("Guess a letter: ")
+def check_guess(guess, word):
+    # Step 2: Convert the guess into lowercase.
+    guess = guess.lower()
 
-    if len(guess) == 1 and guess.isalpha():
-        break
+    # Step 3: Check if the guess is in the word.
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input(word):
+    while True:
+        # Step 2: Move the code for input validation into this function.
+        guess = input("Guess a letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            # Step 3: Call the check_guess function to check if the guess is in the word.
+            check_guess(guess, word)
+            return  # Exit the loop when valid input is provided
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
 
 # Secret word (you can randomly select it as you did before)
 secret_word = "apple"  # Replace with your code to randomly select the secret word
 
-# Step 1: Create an if statement that checks if the guess is in the word.
-if guess in secret_word:
-    # Step 2: In the body of the if statement, print a message saying "Good guess! {guess} is in the word."
-    print(f"Good guess! {guess} is in the word.")
-else:
-    # Step 3: Create an else block that prints a message saying "Sorry, {guess} is not in the word. Try again."
-    print(f"Sorry, {guess} is not in the word. Try again.")
+# Step 4: Call the ask_for_input function to test your code.
+ask_for_input(secret_word)
